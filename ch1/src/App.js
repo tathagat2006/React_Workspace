56495e4c3b88a4469222abb9af23c5b0f053db35
tathagat2006 <tathagat.thapliyal@gmail.com>
2018-08-04 17:12:29 +0530

    addbtn and delbtn working
    Signed-off-by: tathagat2006 <tathagat.thapliyal@gmail.com>

--- ch1/src/App.js
+++ ch1/src/App.js
@@ -5,21 +5,43 @@ import List from './List';
 //you always need to make a new class a react's component class.
 class App extends Component {
 
-    hello() {
-        console.log('use this keyword to call a member function:key concept of difference between member function and function')
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            number:6
+        };
+    }
+    // hello() {
+    //     console.log('use this keyword to call a member function:key concept of difference between member function and function')
+    // }
+    //
+    // componentWillMount() {
+    //     console.log('inside componentWillMount')
+    //     this.hello()
+    // }
+
+    addToList = () => {
+        const {number} = this.state;
+        this.setState({
+            number:number + 1
+        });
     }
 
-    componentWillMount() {
-        console.log('inside componentWillMount')
-        this.hello()
+    subtractToList = () => {
+        const {number} = this.state;
+        this.setState({
+            number:number - 1
+        });
     }
   render() {
+        const number = this.state;
         console.log('in render')
     return (
         <div>
-            <List number={6}/>
-            <button id="addbtn">ADD(+)</button>
-            <button id="delbtn">DELETE(-)</button>
+            <List number={number}/>
+            <button id="addbtn" onClick={this.addToList}>ADD(+)</button>
+            <button id="delbtn" onClick={this.subtractToList}>DELETE(-)</button>
         </div>
     );
   }
@@ -28,3 +50,5 @@ class App extends Component {
 export default App;
 /*number and hello are a part of object called props*/
 //think of <List/> as an object of List clas.
+//state is immutable and on the change of state rerender() is called.
+// .bind() .call() .apply()
