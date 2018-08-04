24004a6be5781c2821d8baa66809c113a045f682
tathagat2006 <tathagat.thapliyal@gmail.com>
2018-08-04 15:49:46 +0530

    changes in app.js
    Signed-off-by: tathagat2006 <tathagat.thapliyal@gmail.com>

--- ch1/src/App.js
+++ ch1/src/App.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+//you always need to make a new class a react's component class.
 class App extends Component {
+
+    hello() {
+        console.log('use this keyword to call a member function:key concept of difference between member function and function')
+    }
+
+    componentWillMount() {
+        console.log('inside componentWillMount')
+        this.hello()
+    }
   render() {
+        console.log('in render')
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <ul>
+            <li>Person 1</li>
+            <li>Person 2</li>
+            <li>Person 3</li>
+            <li>Person 4</li>
+        </ul>
     );
   }
 }
